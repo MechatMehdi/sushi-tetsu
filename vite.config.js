4ce532e20b97fbfbb530f910a6e96cbf0a012644
Mechat Mehdi <mechatmehdi2006@gmail.com>
2026-02-01 16:45:44 +0100

    last last commit

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,8 @@ import react from '@vitejs/plugin-react'
 import tailwindcss from '@tailwindcss/vite'
 
 export default defineConfig({
+    // Add this line:
+    base: "/sushi-tetsu/",
     plugins: [
         react(),
         tailwindcss(),
